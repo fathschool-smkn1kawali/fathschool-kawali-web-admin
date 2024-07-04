2a63a8eced59d05b11db924f89569664b9141374
webfathforce <134572236+webfathforce@users.noreply.github.com>
2024-07-04 17:13:19 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,9 @@ Route::get('/user', [App\Http\Controllers\Api\ApiUserController::class, 'show'])
 //school
 Route::get('/school', [App\Http\Controllers\Api\ApiSchoolController::class, 'show'])->middleware('auth:sanctum');
 
+//school phone
+Route::get('/school-phone', [App\Http\Controllers\Api\ApiSchoolController::class, 'showPhone'])->middleware('auth:sanctum');
+
 //API permission
 Route::apiResource('/api-permissions', App\Http\Controllers\Api\PermissionController::class)->middleware('auth:sanctum');
 
@@ -62,3 +65,9 @@ Route::get('/journal/{id}', [App\Http\Controllers\Api\JournalController::class, 
 
 //update profile
 Route::post('/update-profile', [App\Http\Controllers\Api\AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
+
+//update face
+Route::post('/update-face', [App\Http\Controllers\Api\AuthController::class, 'updateFace'])->middleware('auth:sanctum');
+
+//notifikation
+Route::post('/notification', [App\Http\Controllers\Api\NotificationController::class, 'notification'])->middleware('auth:sanctum');
