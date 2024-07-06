fd6171051b5e4fef46bb3fd7dc66e2b2b4688de6
Fathforce Team <167107476+gofathteam@users.noreply.github.com>
2024-07-06 14:26:45 +0700

    update auth (again)

--- app/Http/Controllers/Api/JournalController.php
+++ app/Http/Controllers/Api/JournalController.php
@@ -7,6 +7,7 @@ use App\Models\Journal;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class JournalController extends Controller
 {
