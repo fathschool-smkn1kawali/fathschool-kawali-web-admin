87231d3e98d9de7193e3e53a7a8f35e54d6e65ce
webfathforce <134572236+webfathforce@users.noreply.github.com>
2024-07-23 22:04:14 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\ClearExpiredSecurityCodes;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,13 +14,27 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->job(new ClearExpiredSecurityCodes())->everyMinute();
+        // Tugas untuk mereset kolom 'is_spaced' menjadi 1 setiap minggu
+        $schedule->call(function () {
+            DB::table('class_routines')->update(['is_spaced' => 1]);
+        })->weeklyOn(0, '23:59'); // Hari Minggu pukul 23:59
 
-        $schedule->command('recurring:invoice')->daily();
+        // Tugas yang akan dijalankan setiap 5 menit
+        $schedule->call(function () {
+            $currentTimeForChecking = now();
+            $endTimeForChecking = $currentTimeForChecking->copy()->addMinutes(6);
+
+            // Periksa dan update jadwal
+            DB::table('class_routines')
+                ->whereBetween('start_time', [$currentTimeForChecking->format('H:i:s'), $endTimeForChecking->format('H:i:s')])
+                ->whereDate('start', '<=', $currentTimeForChecking->format('Y-m-d'))
+                ->whereDate('end', '>=', $currentTimeForChecking->format('Y-m-d'))
+                ->update(['is_spaced' => 0]);
+        })->everyFiveMinutes(); // Menjalankan setiap 5 menit
     }
+
 
     /**
      * Register the commands for the application.
@@ -37,5 +52,5 @@ class Kernel extends ConsoleKernel
         // ...
         'log.activity' => \App\Http\Middleware\LogActivity::class,
     ];
-    
+
 }
