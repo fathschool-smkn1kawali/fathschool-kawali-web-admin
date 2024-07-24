4c0ba89f29e96d8bbec6bf788612f91ad1d9b6bf
Dikri Mahali Ramdani <adm.fathforce@gmail.com>
2024-07-24 20:53:51 +0700

    Update Notification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Kreait\Laravel\Firebase\ServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
